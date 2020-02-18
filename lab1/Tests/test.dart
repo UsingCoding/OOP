7ed42d927f93e3978d57de5d20096ef398e2e9f2
UsingCoding <extendedmoment@outlook.com>
2020-02-18 22:24:49 +0300

    Добавил скриптик тестов

---
+++ lab1/Tests/test.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+Future<void> main() async {
+  List<Map<String, String>> test1 = [
+    {
+      'input': '16 2 1F',
+	  'number': '1',
+      'awaitResult': '11111',
+      'currRes': '',
+    },
+    {
+      'input': '20 10 FF',
+	  'number': '2',
+      'awaitResult': '315',
+      'currRes': '',
+    }
+  ];
+
+  for (var testData in test1) {
+    ProcessResult res = await Process.run('./main', testData['input'].split(' '));
+    if (res.exitCode != 0 || (res.stdout as String).trim() != testData['awaitResult']) {
+			testData['currRes'] = (res.stdout as String).trim();
+		}
+  }
+
+  print(test1);
+
+  for (var testData in test1) {
+	  if (testData['currRes'] != '') {
+		  print("""
+Test #${testData['number']} failed with:
+Input: ${testData['input']}
+Await result: ${testData['awaitResult']}
+Curr result: ${testData['currRes']}""");
+	  }
+	  else{
+	  	print("Test #${testData['number']} passed");
+	  }
+
+	  print("------------------");
+  }
+}
