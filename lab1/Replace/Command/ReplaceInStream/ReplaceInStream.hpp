dc2420aafdd62f4821708140ce73c50b38b8a805
UsingCoding <extendedmoment@outlook.com>
2020-03-21 15:41:37 +0300

    Поправил код Replace: избавился от команды, перенес код в функцию в main.cpp

--- lab1/Replace/Command/ReplaceInStream/ReplaceInStream.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-class ReplaceInStream
-{
-public:
-    void static Execute(std::istream * in, std::ostream * out, std::string searchStr, std::string replaceStr);
-};
--- lab1/Replace/main.cpp
+++ lab1/Replace/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include "Command/ReplaceInStream/ReplaceInStream.hpp"
 
 bool OpenFiles(std::ifstream *fin, std::ofstream *fout, std::string firstFileName, std::string secondFileName);
+void ReplaceInStream(std::istream * in, std::ostream * out, const std::string & searchStr, const std::string & replaceStr);
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 5)
+    if (argc != 5)
     {
-        std::cout << "Not enough arguments" << std::endl;
+        std::cout << "Incorrect count of arguments" << std::endl;
         return 1;
     }
 
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    ReplaceInStream::Execute(&fin, &fout, argv[3], argv[4]);
+    ReplaceInStream(&fin, &fout, argv[3], argv[4]);
 
     return 0;
 }
@@ -31,4 +31,65 @@ bool OpenFiles(std::ifstream *fin, std::ofstream *fout, std::string firstFileNam
     (*fout).open(secondFileName);
 
     return (*fin).is_open() && (*fout).is_open();
+}
+
+void ReplaceInStream(std::istream * in, std::ostream * out, const std::string & searchStr, const std::string & replaceStr)
+{
+    int offset = 0;
+    std::string buffer;
+    std::string line;
+    std::string resLine;
+
+    while (!(*in).eof())
+    {
+        getline(*in, line);
+
+        if (line.length() == 0)
+        {
+            continue;
+        }
+
+        for (size_t i = 0; i < line.length(); i++)
+        {
+            if (offset == searchStr.length() && searchStr.length() != 0)
+            {
+                offset = 0;
+                buffer = "";
+                resLine += replaceStr;
+            }
+
+            if (searchStr.length() != 0 && line[i] == searchStr[offset])
+            {
+                buffer += line[i];
+                ++offset;
+                continue;
+            }
+
+            if (searchStr.length() != 0 && line[i] == searchStr[0])
+            {
+                resLine += buffer;
+                buffer = line[i];
+                offset = 1;
+                continue;
+            }
+
+            if (buffer != "")
+            {
+                resLine += buffer;
+                buffer = "";
+            }
+            resLine += line[i];
+
+            offset = 0;
+        }
+
+        if (buffer != "" && buffer != searchStr)
+        {
+            resLine += buffer;
+            buffer = "";
+        }
+
+        *out << resLine << std::endl;
+        resLine = "";
+    }
 }
