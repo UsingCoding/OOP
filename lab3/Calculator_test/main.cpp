cd8664c36eb657b72f34b2b2043688bcab1f71bd
vadim.makerov <vadim.makerov@solutions.com>
2020-05-20 19:44:18 +0300

    Поправил тесты

--- lab3/Calculator_test/main.cpp
+++ lab3/Calculator_test/main.cpp
@@ -5,12 +5,22 @@
 #include <vector>
 #include <memory>
 #include <sstream>
+#include <algorithm>
 
 #include "../Calculator/NodeBuilder/NodeBuilder.hpp"
 #include "../Calculator/Parser/Parser.hpp"
 #include "../Calculator/Infrastructure/ResourceManger/ResourceManager.hpp"
 #include "../Calculator/Domain/ArithmeticObjects/Variable.hpp"
 #include "../Calculator/Domain/ArithmeticObjects/Function.hpp"
+
+#include "../Calculator/Interpreter/Interpreter.hpp"
+
+std::unique_ptr<Interpreter> GetInterpreter()
+{
+    std::unique_ptr<ResourceManager> manager = std::make_unique<ResourceManager>();
+    std::unique_ptr<NodeBuilder> nodeBuilder = std::make_unique<NodeBuilder>(manager);
+    return std::move(std::make_unique<Interpreter>(nodeBuilder, manager));
+}
 
 void MakeRequest(std::unique_ptr<NodeBuilder> & nodeBuilder, const std::string & userInput)
 {
@@ -210,48 +220,105 @@ SCENARIO("Trying to calculate recursive function")
 
 SCENARIO("Declare and define variables in different cases and without spaces between words and symbols")
 {
-    GIVEN("Gentelman kit(empty resourses)")
+    GIVEN("Gentelman kit(interpreter)")
     {
-        std::unique_ptr<ResourceManager> manager = std::make_unique<ResourceManager>();
-        std::unique_ptr<NodeBuilder> nodeBuilder = std::make_unique<NodeBuilder>(manager);
+        std::unique_ptr<Interpreter> interpreter = GetInterpreter();
 
         WHEN("We declare variable in incorrect way we have error")
         {
             std::string req = "varchar";
-            bool wasError = false;
 
-            try
-            {
-                MakeRequest(nodeBuilder, req);
-            }
-            catch(const std::exception& e)
-            {
-                wasError = true;
-            }
-            
-            REQUIRE(wasError);
+            std::string res = interpreter->Handle(req);
+
+            REQUIRE(res == "Unknown terminal varchar");
         }
 
         WHEN("We define variable without spaces between sign")
         {
             std::string req = "let number=4";
-            bool wasError = false;
 
-            try
-            {
-                MakeRequest(nodeBuilder, req);
-            }
-            catch(const std::exception& e)
-            {
-                wasError = true;
-            }
+            std::string res = interpreter->Handle(req);
             
-            REQUIRE(!wasError);
+            REQUIRE(res == "Success");
         }
 
-        WHEN("")
+        WHEN("We declare some variable")
         {
-            
+            std::string req = "var number";
+            interpreter->Handle(req);
+
+            AND_WHEN("We define same variable name with letter in uppercase")
+            {
+                req = "let NUMBER = 4";
+                std::string res = interpreter->Handle(req);
+
+                THEN("We got no errors")
+                {
+                    REQUIRE(res == "Success");
+                }
+
+                AND_WHEN("We try to get value of var")
+                {
+                    req = "print char";
+                    std::string res = interpreter->Handle(req);
+
+                    THEN("We got NaN")
+                    {
+                        REQUIRE(res == "NaN");
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Calculating Fibonacci number 8 and change first number")
+{
+    GIVEN("Interpreter")
+    {
+        std::unique_ptr<Interpreter> interpreter = GetInterpreter();
+
+        AND_GIVEN("Defined variables and functions")
+        {
+
+            std::vector<std::string> commands = {
+                "let v1=1", 
+                "fn fib0=v0", 
+                "fn fib1=v1", 
+                "fn fib2=fib1+fib0", 
+                "fn fib3=fib2+fib1", 
+                "fn fib4=fib3+fib2", 
+                "fn fib5=fib4+fib3", 
+                "fn fib6=fib5+fib4", 
+                "fn fib7=fib6+fib5", 
+                "fn fib8=fib7+fib6", 
+            };
+
+            std::for_each(commands.begin(), commands.end(), [&interpreter](std::string command){
+                interpreter->Handle(command);
+            });
+
+            THEN("We get number 8")
+            {
+                std::string req = "print fib8";
+                std::string res = interpreter->Handle(req);
+
+                REQUIRE("21" == res);
+            }
+
+            AND_WHEN("We change v1 to 2")
+            {
+                std::string req = "let v1= 2";
+                interpreter->Handle(req);
+
+                THEN("We have number 8 is equal to ")
+                {
+                    req = "print fib8";
+                    std::string res = interpreter->Handle(req);
+
+                    // REQUIRE
+                }
+            }
         }
     }
 }
