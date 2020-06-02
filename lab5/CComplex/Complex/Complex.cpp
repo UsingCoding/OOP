79d4ce4c14a91c98bd83ef2b3217e718c30a25f6
UsingCoding <extendedmoment@outlook.com>
2020-06-02 22:20:25 +0300

    Накидал лабу по комплексным числам

---
+++ lab5/CComplex/Complex/Complex.cpp
@@ -0,0 +1,135 @@
+#include "./Complex.hpp"
+#include "../../../Utils/StringUtils.hpp"
+#include <cmath>
+#include <stdexcept>
+#include <float.h>
+#include <math.h>
+
+#include <iostream>
+
+Complex::Complex(double real, double image): real(real), image(real) {}
+
+double Complex::Re() const
+{
+    return real;
+}
+
+double Complex::Im() const
+{
+    return image;
+}
+
+double Complex::GetMagnitude() const
+{
+    return sqrt(pow(real, 2) + pow(image, 2));
+}
+
+double Complex::GetArgument() const
+{
+    if (real == 0)
+    {
+        throw std::domain_error("You can`t divide by zero");
+    }
+
+    return atan(image/real) * 180.0 / M_PI;
+}
+
+Complex Complex::operator+(const Complex & other)
+{
+    return Complex(real + other.real, image + other.image);
+}
+
+Complex Complex::operator-(const Complex & other)
+{
+    // try with unary operator
+    return Complex(real - other.real, image - other.image);
+}
+
+Complex Complex::operator*(const Complex & other)
+{
+    return Complex(real * other.real - image * other.image, real * other.image + image * other.real);
+}
+
+Complex Complex::operator/(const Complex & other)
+{
+    return Complex(
+        (real * other.real - image * other.image) / (pow(real, 2) + pow(image, 2)),
+        (real * other.image + image * other.real) / (pow(real, 2) + pow(image, 2))
+    );
+}
+
+Complex Complex::operator+()
+{
+    return Complex(real, image);
+}
+Complex Complex::operator-()
+{
+    return Complex(-real, -image);
+}
+
+Complex Complex::operator+=(const Complex & other)
+{
+    real += other.real;
+    image += other.image;
+}
+
+Complex Complex::operator-=(const Complex & other)
+{
+    real -= other.real;
+    image -= other.image;
+}
+
+Complex Complex::operator*=(const Complex & other)
+{
+    real = real * other.real - image * other.image;
+    image = real * other.image + image * other.real;
+}
+
+Complex Complex::operator/=(const Complex & other)
+{
+    real = (real * other.real - image * other.image) / (pow(real, 2) + pow(image, 2));
+    image = (real * other.image + image * other.real) / (pow(real, 2) + pow(image, 2));
+}
+
+bool Complex::operator==(const Complex & other)
+{
+    bool realsAreEqual = fabs(real- other.real) < DBL_EPSILON;
+    bool imagesAreEqual = fabs(image- other.image) < DBL_EPSILON;
+
+    return realsAreEqual && imagesAreEqual;
+}
+
+bool Complex::operator!=(const Complex & other)
+{
+    return !(*this == other);
+}
+
+std::ostream& operator<< (std::ostream &out, const Complex & complex)
+{
+    StringUtils::StringConcatenator result;
+
+    if (complex.real != 0)
+    {
+        if (complex.real < 0)
+        {
+            result << '-';
+        }
+
+        result << complex.real;
+    }
+
+    if (complex.image != 0)
+    {
+        result << (complex.image > 0 ? '+' : '-') << complex.image;
+    }
+
+    return out << (std::string) result << Complex::I;
+}
+
+std::istream& operator>> (std::istream &in, Complex & complex)
+{
+    in >> complex.real;
+    in >> complex.image;
+
+    return in;
+}
---
+++ lab5/CComplex/Complex/Complex.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iosfwd>
+
+class Complex
+{
+private:
+    double real;
+    double image;
+
+	static const char I = 'i';
+public:
+    Complex(double real = 0, double image = 0);
+
+	// возвращает действительную часть комплексного числа
+	double Re()const;
+
+	// возвращает мнимую часть комплексного числа
+	double Im()const;
+
+	// возвращает модуль комплексного числа
+	double GetMagnitude()const;
+
+	// возвращает аргумент комплексного числа
+	double GetArgument()const;
+
+	Complex operator+(const Complex & other);
+
+	Complex operator-(const Complex & other);
+
+	Complex operator*(const Complex & other);
+
+	Complex operator/(const Complex & other);
+
+	Complex operator+();
+	Complex operator-();
+
+	Complex operator+=(const Complex & other);
+
+	Complex operator-=(const Complex & other);
+
+	Complex operator*=(const Complex & other);
+
+	Complex operator/=(const Complex & other);
+
+	bool operator==(const Complex & other);
+
+	bool operator!=(const Complex & other);
+
+	friend std::ostream& operator<< (std::ostream &out, const Complex & complex);
+	friend std::istream& operator>> (std::istream &in, Complex & complex);
+};
